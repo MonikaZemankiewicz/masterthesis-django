9de6b20a9f48f9712290465c03f9a40442dadde3
Monika <monika.zemankiewicz.2015@gmail.com>
2023-05-30 03:46:34 +0100

    basic views and urls

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='landing-page-home'),
-    path('about/', views.about, name='landing-page-about'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'about.html')
