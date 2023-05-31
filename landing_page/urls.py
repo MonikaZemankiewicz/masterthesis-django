d0e8abbb50b34eef4bde66564c7b9bd1a0cdc6cb
Monika <monika.zemankiewicz.2015@gmail.com>
2023-05-31 01:28:42 +0100

    navbar

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contact/', views.contact, name='contact'),
+    
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Picture
+
+
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'landing_page/home.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'landing_page/about.html')
+
+def gallery(request):
+    pictures = Picture.objects.all()
+    context = {'pictures': pictures}
+    return render(request, 'landing_page/gallery.html', context)
+
+def contact(request):
+    return render(request, 'landing_page/contact.html')
