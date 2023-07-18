de4e8c7746155c2905adb6028acb5d95dc6b456b
Monika <monika.zemankiewicz.2015@gmail.com>
2023-07-18 03:40:24 +0200

    image optimization

--- landing_page/models.py
+++ landing_page/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from optimized_image.fields import OptimizedImageField
 
 
 class Picture(models.Model):
     name = models.CharField(max_length=200)
-    image = models.ImageField(default='1.jpg')
+    image = OptimizedImageField(default='1.jpg')
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
